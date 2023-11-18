5ad5bb55305d85691cd4257096df5b0d1128b353
Richard Zhong <98974633+Jz8801@users.noreply.github.com>
2023-11-18 15:47:23 -0500

    webcam and upload

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, session, url_for, redirect, j
 import json
 from google.cloud import vision
 import os
+import base64
 
 
 app = Flask(__name__)
@@ -12,19 +13,21 @@ client = vision.ImageAnnotatorClient()
 def index():
     return render_template('index.html')
 
-@app.route('/upload', methods=['POST', 'GET'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    print("upload route reached")
-    file = request.files.get('file')
-    if not file:
-        return 'No file uploaded.', 400
-    
-    content = file.read()
-    image = vision.Image(content=content)
-    response = client.label_detection(image=image)
-    labels = response.label_annotations
+    image_data = request.form.get('imageData')
+    if image_data:
+        # Strip the header from the data URL
+        header, image_data = image_data.split(',', 1)
+        # Convert to bytes
+        image_bytes = base64.b64decode(image_data)
+        image = vision.Image(content=image_bytes)
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+        return jsonify([label.description for label in labels])
+    else:
+        return 'No image data received.', 400
 
-    return jsonify([label.description for label in labels])
 
 if __name__ == '__main__':
     app.run(debug=True)
