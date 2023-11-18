9dcb1db637e50ad593561c876c4c2dc366daddfe
Richard Zhong <98974633+Jz8801@users.noreply.github.com>
2023-11-18 15:16:14 -0500

    blablabal

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
-from flask import Flask, render_template, request, session, url_for, redirect
-import pymysql.cursors
+from flask import Flask, render_template, request, session, url_for, redirect, jsonify
+import json
+from google.cloud import vision
+import os
 
+
+app = Flask(__name__)
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './Key.json'
+client = vision.ImageAnnotatorClient()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    print("upload route reached")
+    file = request.files.get('file')
+    if not file:
+        return 'No file uploaded.', 400
+    
+    content = file.read()
+    image = vision.Image(content=content)
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+
+    return jsonify([label.description for label in labels])
+
+if __name__ == '__main__':
+    app.run(debug=True)
