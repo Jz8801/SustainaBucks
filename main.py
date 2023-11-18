f6e3d6023bcd90b95232bb98971fd56f36050fd9
Richard Zhong <98974633+Jz8801@users.noreply.github.com>
2023-11-18 17:36:50 -0500

    fixed only webcam working

--- main.py
+++ main.py
@@ -15,18 +15,27 @@ def index():
 
 @app.route('/upload', methods=['POST'])
 def upload():
-    image_data = request.form.get('imageData')
-    if image_data:
+    image_data_webcam = request.form.get('imageData')
+    image_data_upload = request.files.get('file')
+    
+    if image_data_webcam:
         # Strip the header from the data URL
-        header, image_data = image_data.split(',', 1)
+        header, image_data = image_data_webcam.split(',', 1)
         # Convert to bytes
         image_bytes = base64.b64decode(image_data)
         image = vision.Image(content=image_bytes)
         response = client.label_detection(image=image)
         labels = response.label_annotations
         return jsonify([label.description for label in labels])
+    elif image_data_upload:
+        content = image_data_upload.read()
+        image = vision.Image(content=content)
+        response = client.label_detection(image=image)
+        labels = response.label_annotations
+        return jsonify([label.description for label in labels])
     else:
         return 'No image data received.', 400
+
 
 
 if __name__ == '__main__':
