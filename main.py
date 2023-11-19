c5d5ea419f1b9e077d5163169f90a85aabebac1d
Richard Zhong <98974633+Jz8801@users.noreply.github.com>
2023-11-18 21:32:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -23,18 +23,34 @@ def upload():
         # Convert to bytes
         image_bytes = base64.b64decode(image_data)
         image = vision.Image(content=image_bytes)
-        response = client.label_detection(image=image)
-        labels = response.label_annotations
-        return jsonify([label.description for label in labels])
+        # response = client.label_detection(image=image)
+        # labels = response.label_annotations
+        # return jsonify([label.description for label in labels])
     elif image_data_upload:
         content = image_data_upload.read()
         image = vision.Image(content=content)
-        response = client.label_detection(image=image)
-        labels = response.label_annotations
-        return jsonify([label.description for label in labels])
+        # response = client.label_detection(image=image)
+        # labels = response.label_annotations
+        # return jsonify([label.description for label in labels])
     else:
         return 'No image data received.', 400
+    
+    response_label = client.label_detection(image=image)
+    labels = response_label.label_annotations
 
+    response_logo = client.logo_detection(image=image)
+    logos = response_logo.logo_annotations
+
+    response_text = client.text_detection(image=image)
+    texts = response_text.text_annotations
+
+    results = {
+        'labels': [label.description for label in labels],
+        'logos': [logo.description for logo in logos],
+        'text': [text.description for text in texts]
+    }
+
+    return jsonify(results)
 
 
 if __name__ == '__main__':
